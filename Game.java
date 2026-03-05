7a13ab1375d5647479ff886e453a17f70bc144ef
Louisse <154320354+louissellerena@users.noreply.github.com>
2026-03-05 16:39:24 +0400

    Refactor Game class with new features and mechanics
    Updated game mechanics, added death screen and mouse controls.

--- Game.java
+++ Game.java
@@ -1,213 +1,233 @@
 import java.awt.*;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 import java.util.ArrayList;
 import game2D.*;
 
-// Game demonstrates how we can override the GameCore class
-// Student ID: 3359480
+//Game demonstrates how we can override the GameCore class
+//to create our own 'game'. We usually need to implement at
+//least 'draw' and 'update' (not including any local event handling)
+//to begin the process. You should also add code to the 'init'
+//method that will initialise event handlers etc. 
 
+//Student ID: 3359480
 @SuppressWarnings("serial")
-public class Game extends GameCore 
+public class Game extends GameCore implements MouseListener
 {
 	// Useful game constants
-	static int screenWidth = 512;
-	static int screenHeight = 384;
-
-	// Game constants
-    float 	lift = 0.005f;
-    float	gravity = 0.0001f;
-    float	fly = -0.04f;
-    float	moveSpeed = 0.05f;
+    static int screenWidth = 800; 
+    static int screenHeight = 600;
     
-    // Game state flags
-    boolean moveRight = false;
-    boolean moveLeft = false;
-    boolean onGround = false;
-    boolean jumpPressed = false;
-    boolean debug = true;		
-
-    // Game resources
-    Animation landing, walk, walkR;
+    // Game constants
+    float gravity = 0.0001f;
+    float moveSpeed = 0.05f;
     
-    Sprite	player = null;
-    ArrayList<Sprite> 	clouds = new ArrayList<Sprite>();
-    ArrayList<Tile>		collidedTiles = new ArrayList<Tile>();
-
-    TileMap tmap = new TileMap();	// Our tile map
+    //Game state flags
+    boolean moveRight = false, moveLeft = false, onGround = false, jumpPressed = false, debug = false;
+    boolean isAlive = true;
+    boolean showDeathScreen = false; 
+    //Game resources
+    Animation landing, walk, walkR, jump, jumpL, death;
     
-    long total;
+    Sprite player = null;
+    ArrayList<Sprite> clouds = new ArrayList<Sprite>();
+    ArrayList<Tile> collidedTiles = new ArrayList<Tile>();
+    
+    TileMap tmap = new TileMap(); // Our tile map, note that we load it in init()
+
+    long total; 				// The score will be the total time elapsed since a crash
+    
+    // UI Button - Centered on screen
+    Rectangle restartBtn = new Rectangle(screenWidth/2 - 50, screenHeight/2 + 20, 100, 40);
 
     public static void main(String[] args) {
         Game gct = new Game();
         gct.init();
-        gct.run(false,screenWidth,screenHeight);
+        gct.run(false, screenWidth, screenHeight);
     }
 
     public void init()
-    {         
-        Sprite s;
+    {          
         tmap.loadMap("maps", "level1.txt");
-        
-        setSize(tmap.getPixelWidth()/4, tmap.getPixelHeight());
+        setSize(screenWidth, screenHeight);
         setVisible(true);
+        
+        addMouseListener(this); 
         
         landing = new Animation();
         landing.loadAnimationFromSheet("images/Lidle.png", 2, 1, 300);
-
+        
         walk = new Animation();
         walk.loadAnimationFromSheet("images/Lwalk.png", 6, 1, 100);
         
         walkR = new Animation();
         walkR.loadAnimationFromSheet("images/Walk.png", 6, 1, 100);
 
+        jump = new Animation();
+        jump.loadAnimationFromSheet("images/Ljump.png", 2, 1, 1000);
+        
+        jumpL = new Animation();
+        jumpL.loadAnimationFromSheet("images/Jump.png", 2, 1, 1000);
+
+        death = new Animation();
+        death.loadAnimationFromSheet("images/Death.png", 1, 1, 150); 
+        death.setLoop(false); 
+
         player = new Sprite(landing);
         player.setScale(2.0f);
         
         Animation ca = new Animation();
         ca.addFrame(loadImage("images/cloud.png"), 1000);
-        
-        for (int c=0; c<3; c++)
-        {
-        	s = new Sprite(ca);
-        	s.setX(screenWidth + (int)(Math.random()*200.0f));
-        	s.setY(30 + (int)(Math.random()*150.0f));
-        	s.setVelocityX(-0.02f);
-        	s.show();
-        	clouds.add(s);
+        for (int c=0; c<6; c++) {
+            Sprite s = new Sprite(ca);
+            s.setX((int)(Math.random() * tmap.getPixelWidth()));
+            s.setY(50 + (int)(Math.random() * 200.0f));
+            s.setVelocityX(-0.01f);
+            s.show();
+            clouds.add(s);
         }
-
         initialiseGame();
     }
 
-    public void initialiseGame()
-    {
+    public void initialiseGame() {
     	total = 0;
-        player.setPosition(200,200);
-        player.setVelocity(0,0);
+        isAlive = true;
+        showDeathScreen = false; 
+        player.setPosition(200, 200);
+        player.setVelocity(0, 0);
+        player.setAnimation(landing);
         player.show();
     }
     
-    public void draw(Graphics2D g)
-    {    	
-        int xo = -(int)player.getX() + 200;
-        int yo = -(int)player.getY() + 200;
+    public void draw(Graphics2D g) {       
+        int xo = -(int)player.getX() + (screenWidth / 2);
+        int yo = -(int)player.getY() + (screenHeight / 2);
 
-        g.setColor(Color.white);
+        // Sky background
+        g.setColor(new Color(135, 206, 235)); 
         g.fillRect(0, 0, getWidth(), getHeight());
         
-        for (Sprite s: clouds)
-        {
-        	s.setOffsets(xo,yo);
-        	s.draw(g);
+        for (Sprite s: clouds) {
+            s.setOffsets((int)(xo * 0.5f), yo); 
+            s.draw(g);
         }
 
-        tmap.draw(g,xo,yo); 
-
+        tmap.draw(g, xo, yo); 
         player.setOffsets(xo, yo);
         player.draw(g);
         
-        if (debug)
-        {
-            tmap.drawBorder(g, xo, yo, Color.black);
+        String msg = String.format("Score: %d", total/100);
+        g.setColor(Color.darkGray);
+        g.setFont(new Font("Arial", Font.BOLD, 16));
+        g.drawString(msg, getWidth() - 100, 30);
+        
+        if (debug) {
             g.setColor(Color.red);
-        	player.drawBoundingBox(g);
-        	drawCollidedTiles(g, tmap, xo, yo);
+            player.drawBoundingBox(g);
+            drawCollidedTiles(g, tmap, xo, yo);
+        }
+
+        // ui drawn last to be on top
+        if (showDeathScreen) {
+            drawDeathUI(g);
         }
     }
 
-    public void drawCollidedTiles(Graphics2D g, TileMap map, int xOffset, int yOffset)
-    {
-		if (collidedTiles.size() > 0)
-		{	
-			int tileWidth = map.getTileWidth();
-			int tileHeight = map.getTileHeight();
-			g.setColor(Color.blue);
-			for (Tile t : collidedTiles)
-				g.drawRect(t.getXC()+xOffset, t.getYC()+yOffset, tileWidth, tileHeight);
-		}
+    private void drawDeathUI(Graphics2D g) {
+        // Overlay
+        g.setColor(new Color(0, 0, 0, 180));
+        g.fillRect(0, 0, screenWidth, screenHeight);
+
+        // Text
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Arial", Font.BOLD, 60));
+        String msg = "YOU DIED";
+        int strWidth = g.getFontMetrics().stringWidth(msg);
+        g.drawString(msg, (screenWidth - strWidth) / 2, screenHeight / 2 - 20);
+
+        // Button
+        g.setColor(Color.RED);
+        g.fill(restartBtn);
+        g.setColor(Color.WHITE);
+        g.draw(restartBtn);
+        g.setFont(new Font("Arial", Font.BOLD, 18));
+        g.drawString("Restart", restartBtn.x + 18, restartBtn.y + 26);
     }
-	
-    public void update(long elapsed)
-    {
-        onGround = false;
+
+    public void update(long elapsed) {
+        if (!isAlive) {
+            player.update(elapsed);
+            // If the death animation has played through once
+            if (death.hasLooped()) { 
+                showDeathScreen = true;
+            }
+            return;
+        }
         
-        // FIXED ERROR: Changed setVelocityY() call inside to getVelocityY()
+        total += elapsed; //increment score while alive
+
         player.setVelocityY(player.getVelocityY() + (gravity * elapsed));
         
+        // Handle Animations properly
         if (moveRight) {
-            player.setAnimation(walk); 
             player.setVelocityX(moveSpeed);
+            if (!onGround) { if (player.getAnimation() != jump) player.setAnimation(jump); }
+            else { if (player.getAnimation() != walk) player.setAnimation(walk); }
             player.setScale(2.0f, 2.0f); 
-        } 
-        else if (moveLeft) {
-            player.setAnimation(walkR); 
+        } else if (moveLeft) {
             player.setVelocityX(-moveSpeed);
+            if (!onGround) { if (player.getAnimation() != jumpL) player.setAnimation(jumpL); }
+            else { if (player.getAnimation() != walkR) player.setAnimation(walkR); }
             player.setScale(-2.0f, 2.0f); 
-        } 
-        else {
+        } else {
             player.setVelocityX(0);
-            player.setAnimation(landing);
+            if (!onGround) { if (player.getAnimation() != jump) player.setAnimation(jump); }
+            else { if (player.getAnimation() != landing) player.setAnimation(landing); }
             player.setScale(2.0f, 2.0f);
         }
         
-       	for (Sprite s: clouds) s.update(elapsed);
+        for (Sprite s: clouds) s.update(elapsed);
         player.update(elapsed);
-       
         handleScreenEdge(player, tmap, elapsed);
         checkTileCollision(player, tmap);
         
         if (jumpPressed && onGround) {
-            player.setVelocityY(-0.2f);
+            player.setVelocityY(-0.25f);
+            onGround = false;
             jumpPressed = false;
         }
     }
     
-    public void handleScreenEdge(Sprite s, TileMap tmap, long elapsed)
-    {
-    	float difference = s.getY() + s.getHeight() - tmap.getPixelHeight();
-        if (difference > 0)
-        {
-        	s.setY(tmap.getPixelHeight() - s.getHeight() - (int)(difference)); 
-        	s.setVelocityY(-s.getVelocityY()*0.75f);
+    public void handleScreenEdge(Sprite s, TileMap tmap, long elapsed) {
+        if (s.getY() + 32.0f > tmap.getPixelHeight()) {
+            if (isAlive) {
+                isAlive = false;
+                s.setVelocity(0, 0);
+                death.start(); // Resets animation to frame 0
+                s.setAnimation(death);
+            }
         }
     }
-    
-    public void keyPressed(KeyEvent e) 
-    { 
-    	int key = e.getKeyCode();
-		switch (key)
-		{
-			case KeyEvent.VK_UP: jumpPressed = true; break;
-			case KeyEvent.VK_RIGHT: moveRight = true; break;
-			case KeyEvent.VK_LEFT: moveLeft = true; break;
-			case KeyEvent.VK_ESCAPE: stop(); System.exit(0); break; // FIXED: Added System.exit to clear icons
-			case KeyEvent.VK_B: debug = !debug; break;
-		}
-    }
 
-    public void checkTileCollision(Sprite s, TileMap tmap)
-    {
+    public void checkTileCollision(Sprite s, TileMap tmap) {
         collidedTiles.clear();
-        float sx = s.getX();
-        float sy = s.getY();
-        float swidth = s.getWidth() * 0.5f; // Tighter collision box for immediate falling
-        float sheight = s.getHeight();
-        float offsetX = (s.getWidth() - swidth) / 2;
-        
-        float tileWidth = tmap.getTileWidth();
-        float tileHeight = tmap.getTileHeight();
+        float sx = s.getX(), sy = s.getY();
+        float pSize = 32.0f; 
+        float swidth = pSize * 0.8f; 
+        float offsetX = (pSize - swidth) / 2;
+        float tileWidth = tmap.getTileWidth(), tileHeight = tmap.getTileHeight();
         
         int xL = (int) ((sx + offsetX) / tileWidth);
         int xR = (int) ((sx + offsetX + swidth - 1) / tileWidth);
-        int yB = (int) ((sy + sheight) / tileHeight);
+        int xMid = (int) ((sx + (pSize / 2)) / tileWidth); 
+        int yB = (int) ((sy + pSize) / tileHeight); 
         
-        Tile fL = tmap.getTile(xL, yB);
-        Tile fR = tmap.getTile(xR, yB);
+        Tile fL = tmap.getTile(xL, yB), fR = tmap.getTile(xR, yB), fMid = tmap.getTile(xMid, yB);
         
+        onGround = false; 
         if (s.getVelocityY() >= 0) {
-            if ((fL != null && fL.getCharacter() != '.') || (fR != null && fR.getCharacter() != '.')) {
-                Tile t = (fL != null && fL.getCharacter() != '.') ? fL : fR;
-                s.setY(t.getYC() - sheight);
+            if ((fL != null && fL.getCharacter() != '.') || (fR != null && fR.getCharacter() != '.') || (fMid != null && fMid.getCharacter() != '.')) {
+                Tile t = (fMid != null && fMid.getCharacter() != '.') ? fMid : (fL != null && fL.getCharacter() != '.') ? fL : fR;
+                s.setY(t.getYC() - pSize); 
                 s.setVelocityY(0); 
                 onGround = true;
                 collidedTiles.add(t);
@@ -215,10 +235,40 @@ public class Game extends GameCore
         }
     }
 
-	public void keyReleased(KeyEvent e) { 
-		int key = e.getKeyCode();
-		if (key == KeyEvent.VK_UP) jumpPressed = false;
-		if (key == KeyEvent.VK_RIGHT) moveRight = false;
-		if (key == KeyEvent.VK_LEFT) moveLeft = false;
-	}
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // Detect click on restart button when screen is active
+        if (showDeathScreen && restartBtn.contains(e.getPoint())) {
+            initialiseGame();
+        }
+    }
+
+    @Override public void mousePressed(MouseEvent e) {}
+    @Override public void mouseReleased(MouseEvent e) {}
+    @Override public void mouseEntered(MouseEvent e) {}
+    @Override public void mouseExited(MouseEvent e) {}
+
+    public void keyPressed(KeyEvent e) { 
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_UP) jumpPressed = true;
+        if (key == KeyEvent.VK_RIGHT) moveRight = true;
+        if (key == KeyEvent.VK_LEFT) moveLeft = true;
+        if (key == KeyEvent.VK_ESCAPE) { stop(); System.exit(0); }
+        if (key == KeyEvent.VK_B) debug = !debug;
+    }
+
+    public void keyReleased(KeyEvent e) { 
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_UP) jumpPressed = false;
+        if (key == KeyEvent.VK_RIGHT) moveRight = false;
+        if (key == KeyEvent.VK_LEFT) moveLeft = false;
+    }
+
+    public void drawCollidedTiles(Graphics2D g, TileMap map, int xOffset, int yOffset) {
+        if (collidedTiles.size() > 0) {   
+            g.setColor(Color.blue);
+            for (Tile t : collidedTiles)
+                g.drawRect(t.getXC() + xOffset, t.getYC() + yOffset, map.getTileWidth(), map.getTileHeight());
+        }
+    }
 }
